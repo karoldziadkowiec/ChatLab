776f22e0d030e9f4f4b8ddf8e44d7acc97a3b017
karoldziadkowiec <karol.dziadkowiec@gmail.com>
2026-04-10 21:09:35 +0200

    simulation config changed

--- src/WebClient/src/config/SimulationConfig.tsx
+++ src/WebClient/src/config/SimulationConfig.tsx
@@ -1,8 +1,8 @@
 export const MessageContentConfig: string = 'Wiadomość123456789!';
 // 60000 = 1min
-export const MessageIntervalConfigInMs: number = 100;
+export const MessageIntervalConfigInMs: number = 10000;
 // If <= 0, simulation runs until manually stopped.
-export const SimulationTimeConfigInMs: number = 10000;
+export const SimulationTimeConfigInMs: number = 50000;
 
 export default {
 	MessageContentConfig,
